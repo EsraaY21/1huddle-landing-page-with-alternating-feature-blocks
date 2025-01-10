cb445567093985cb1f5c09f46e1660ff2b787d37
Esra <esraayaareb@gmail.com>
2025-01-11 01:05:08 +0300

    done

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function CallToAction() {
   return (
-    <div className="cta">
-      <h2>Ready To Build Your Community?</h2>
+    <div className="container">
+      <div className="cta">
+        <h2>Ready To Build Your Community?</h2>
 
-      <div>
-        <a>Get Started For Free</a>
+        <div>
+          <a>Get Started For Free</a>
+        </div>
       </div>
     </div>
   );
